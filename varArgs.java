5b5d0eb5886db80c75d3a70844c7bbcaa36b62a7
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2026-01-03 15:45:15 +0530

    variable arguments of mix datatypes

--- varArgs.java
+++ varArgs.java
@@ -3,10 +3,18 @@ import java.util.Arrays;
 public class varArgs {
 
     public static void main(String[] args) {
+
         fun(2,3,4,5,6,7,8,9);  // gets stored as array- collection of integers
+        multiple(10,20,"Sakshi","Kaveri");
     }
 
     static void fun(int ...v) {
         System.out.println(Arrays.toString(v));
+    }
+
+    static void multiple(int a, int b, String ...c){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(Arrays.toString(c));
     }
 }
