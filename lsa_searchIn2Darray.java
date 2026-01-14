e7d77a85b062d82405799de7cee7719aa1b99c64
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2026-01-15 00:01:07 +0530

    lsa- finding minimum in 2D array

--- lsa_searchIn2Darray.java
+++ lsa_searchIn2Darray.java
@@ -4,11 +4,13 @@ import java.util.Arrays;
 public class lsa_searchIn2Darray {
 
     public static void main(String[] args) {
-        int[][] arr = {{1, 2, 3, 4}, {1, 5, 6, 7, 8}, {11, 22, 33, 44, 55}};
+        int[][] arr = {{1, 2, 3, 4}, {1, -5, 6, 7, 8}, {11, 22, 33, 44, 55}};
         int target = 22;
 
         int[] result = searchIn2DArray(arr, target);
         System.out.println(Arrays.toString(result));
+
+        System.out.println(min(arr));
     }
 
     private static int[] searchIn2DArray(int[][] arr, int target) {
@@ -21,5 +23,18 @@ public class lsa_searchIn2Darray {
             }
         }
         return new int[]{-1, -1};
+    }
+
+    private static int min(int[][] arr) {
+        int min = Integer.MAX_VALUE;
+        for (int[] i : arr) {
+            for (int j : i) {
+                if (j < min) {
+                    min = j;
+                }
+                
+            }
+        }
+        return min;
     }
 }
