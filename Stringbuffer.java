6b67317a3ad654144a9cc6304ca2a8b60681d367
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2026-01-17 14:26:47 +0530

    generating random string using stringbuffer

--- Stringbuffer.java
+++ Stringbuffer.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Stringbuffer {
     public static void main(String[] args) {
 
@@ -23,5 +25,21 @@ public class Stringbuffer {
         String str = sb.toString();
         System.out.println(str);
 
+        // Generating random string
+        int n = 25;
+        String result = generateString(n);
+        System.out.println(result);
+    }
+
+    static String generateString(int size) {
+        StringBuffer sb = new StringBuffer(size);
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            // System.out.println(random.nextFloat()*26);
+            int randomChar = 97 + (int) (random.nextFloat() * 26);
+            sb.append((char) randomChar);
+        }
+        return sb.toString();
     }
 }
