a1bbf432e80d8c5b4a42491b40739b9498a570b4
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2026-03-19 09:07:20 +0530

    factorial using recursion

---
+++ Recursion/Factorial.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.print(factorial(5));
+    }
+
+    static int factorial(int n) {
+        if (n < 1) return 1;
+        return n * factorial(n - 1);
+    }
+}
